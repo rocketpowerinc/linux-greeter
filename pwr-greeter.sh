0fda56cc98527c2fe15f65bb65c73cd11ff0c809
rocketpowerinc <rocketpowerinc@proton.me>
2025-02-17 20:20:42 -0500

    almost perfect

--- pwr-greeter.sh
+++ pwr-greeter.sh
@@ -10,27 +10,35 @@ rm -rf "$DOWNLOAD_PATH"
 # Clone repo
 git clone "$REPO_URL" "$DOWNLOAD_PATH"
 
+# chmod +x
+find $DOWNLOAD_PATH -type f -name "*.sh" -exec chmod +x {} \;
+
 # Check if the clone was successful
 if [ $? -ne 0 ]; then
     zenity --error --text="Failed to clone the repository."
     exit 1
 fi
 
-# Create the Zenity dialog with a list of options
-CHOICE=$(zenity --list --radiolist --title="Choose a script to run" --column="Select" --column="Script" TRUE "APT Script" FALSE "Flatpak Script")
+# Present a list of options to the user
+CHOICE=$(zenity --list --title="Choose a script to run" --column="Scripts" \
+    "APT Manager" \
+    "Flatpak Manager" \
+    --width=500 --height=500)
 
 # Check the user's choice
-if [ "$CHOICE" = "APT Script" ]; then
-    # Run button_packages_apt.sh
-    bash "$DOWNLOAD_PATH/button_packages_apt.sh"
-elif [ "$CHOICE" = "Flatpak Script" ]; then
-    # Run button_packages_flatpak.sh
-    bash "$DOWNLOAD_PATH/button_packages_flatpak.sh"
-else
-    # If the user closes the dialog, show a goodbye message
-    zenity --info --text="Goodbye!"
-    exit 0
-fi
+case "$CHOICE" in
+    "APT Manager")
+        bash "$DOWNLOAD_PATH/button_packages_apt.sh"
+        ;;
+    "Flatpak Manager")
+        bash "$DOWNLOAD_PATH/button_packages_flatpak.sh"
+        ;;
+    *)
+        # If the user closes the dialog or cancels, show a Cancel dialog
+        zenity --info --text="Canceled by user."
+        exit 0
+        ;;
+esac
 
 # Clean up
 rm -rf "$DOWNLOAD_PATH"
