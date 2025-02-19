30b25e8f01ce938a1d694fe9f291edd34f8288ba
rocketpowerinc <rocketpowerinc@proton.me>
2025-02-18 20:33:57 -0500

    lazyvim

--- button_install_lazyvim.sh
+++ button_install_lazyvim.sh
@@ -5,7 +5,22 @@ install_dependencies() {
   if [ -f /etc/debian_version ]; then
     # Debian-based
     sudo apt update && sudo apt upgrade -y
-    sudo apt install -y neovim git lazygit fzf ripgrep fd-find
+    sudo apt install -y git fzf ripgrep fd-find
+
+    #todo Install Neovim from source (because debian is to out of date as of Feb 2025)
+    sudo apt install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+    git clone https://github.com/neovim/neovim.git
+    cd neovim
+    git checkout stable
+    make CMAKE_BUILD_TYPE=Release
+    sudo make install
+
+
+    #lazygit
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+    tar xf lazygit.tar.gz lazygit
+    sudo install lazygit -D -t /usr/local/bin/
   elif [ -f /etc/fedora-release ]; then
     # Fedora-based
     sudo dnf update -y
@@ -26,14 +41,15 @@ install_dependencies() {
 
 # Backup old Neovim configuration
 backup_old_config() {
-  mv ~/.config/nvim{,.bak}
-  mv ~/.local/share/nvim{,.bak}
-  mv ~/.local/state/nvim{,.bak}
-  mv ~/.cache/nvim{,.bak}
+  mv -f ~/.config/nvim{,.bak} 2>/dev/null
+  mv -f ~/.local/share/nvim{,.bak} 2>/dev/null
+  mv -f ~/.local/state/nvim{,.bak} 2>/dev/null
+  mv -f ~/.cache/nvim{,.bak} 2>/dev/null
 }
 
 # Clone LazyVim repository
 setup_neovim() {
+  [ -d ~/.config/nvim ] && rm -rf ~/.config/nvim
   git clone https://github.com/LazyVim/starter ~/.config/nvim
   rm -rf ~/.config/nvim/.git
 }
