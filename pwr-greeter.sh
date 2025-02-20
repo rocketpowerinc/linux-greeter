0bad00bc49998f149552c17dc1755374fd96f59e
rocketpowerinc <rocketpowerinc@proton.me>
2025-02-19 19:58:30 -0500

    s

--- pwr-greeter.sh
+++ pwr-greeter.sh
@@ -49,7 +49,7 @@ case "$CHOICE" in
         ;;
     *)
         # If the user closes the dialog or cancels, show a Cancel dialog
-        #zenity --info --text="Canceled by user."
+        #zenity --info --text="Canceled by user." #*this was annoying
         exit 0
         ;;
 esac
