67f49119fec038f5c972c6ff1c3960f0e9b53ed6
rocketpowerinc <rocketpowerinc@proton.me>
2025-02-21 01:41:45 -0500

    s

--- button_open_tituslinutil.sh
+++ button_open_tituslinutil.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-#bash -c 'flatpak run com.raggesilver.BlackBox --command="bash -c \"curl -fsSL https://christitus.com/linux | sh\""'
-
 # First try to run the Flatpak command
 if flatpak run com.raggesilver.BlackBox --command="bash -c 'curl -fsSL https://christitus.com/linux | sh'"; then
   echo "Executed via Flatpak."
