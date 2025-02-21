c43c313b2c0249aeb781181e7f595b40e4471727
rocketpowerinc <rocketpowerinc@proton.me>
2025-02-21 01:36:26 -0500

    s

--- button_open_tituslinutil.sh
+++ button_open_tituslinutil.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-bash -c 'curl -fsSL https://christitus.com/linux | sh'
+#bash -c 'flatpak run com.raggesilver.BlackBox --command="bash -c \"curl -fsSL https://christitus.com/linux | sh\""'
+curl -fsSL https://christitus.com/linux | sh
