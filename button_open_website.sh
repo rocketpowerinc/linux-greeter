a43de4d97c88c24193f4d2b95b3fae11e8c89b0b
rocketpowerinc <rocketpowerinc@proton.me>
2025-02-21 19:56:44 -0500

    s

---
+++ button_open_website.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+xdg-open https://rocketdashboard.notion.site/Rocket-Power-Included-1a1627bc6fd8805ab693f164a1b3ceda?pvs=4
+
+#* gio only works in gnome
+#gio open https://rocketdashboard.notion.site/Rocket-Power-Included-1a1627bc6fd8805ab693f164a1b3ceda?pvs=4
