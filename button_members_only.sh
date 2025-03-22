49adab4b3600192d4f47d21c566d446bac214176
Rocket <adamrivard87@gmail.com>
2025-03-22 15:11:36 -0400

    s

--- button_members_only.sh
+++ button_members_only.sh
@@ -5,8 +5,7 @@ yad --title="" \
   --width=600 --height=600 \
   --form --columns=2 --align=center --no-buttons --dark \
   --text-align=center --text="<span size='x-large' foreground='grey'>Members Menu</span>\n\n\n" \
-  --field="<span foreground='#FFFFFF'>$(printf '\uF269')</span>     MX-Spins":FBTN "bash -c 'echo test'" \
+  --field="<span foreground='#FFFFFF'>$(printf '\uF269')</span>     MX-Spins":FBTN "bash -c '$DOWNLOAD_PATH/button_members_mxspins.sh'" \
   --field="<span foreground='#FFFFFF'>$(printf '\uF0C7')</span>     Place Holder":FBTN "bash -c 'echo test'"
-
 
 choice=$?
