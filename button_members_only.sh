324ff3f5b66ec84a9e60bbbfeb32e83cc3da7608
Rocket <adamrivard87@gmail.com>
2025-03-22 15:43:01 -0400

    s

--- button_members_only.sh
+++ button_members_only.sh
@@ -6,8 +6,8 @@ DOWNLOAD_PATH="$HOME/Downloads/linux-greeter"
 yad --title="" \
   --width=600 --height=600 \
   --form --columns=2 --align=center --no-buttons --dark \
-  --text-align=center --text="<span size='x-large' foreground='grey'>Members Menu</span>\n\n\n" \
-  --field="<span foreground='#FFFFFF'>$(printf '\uF269')</span>     MX-Spins":FBTN "bash -c '$DOWNLOAD_PATH/button_members_mxspins.sh'" \
-  --field="<span foreground='#FFFFFF'>$(printf '\uF0C7')</span>     Place Holder":FBTN "bash -c 'echo test'"
+  --text-align=center --text="<span size='x-large'>Members Menu</span>\n\n\n" \
+  --field="🌪️     MX-Spins":FBTN "bash -c '$DOWNLOAD_PATH/button_members_mxspins.sh'" \
+  --field="🌪️     Place Holder":FBTN "bash -c 'echo test'"
 
 choice=$?
