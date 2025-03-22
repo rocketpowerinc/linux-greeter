2589d07e0489d193b044ea0a2d4941bf5918b199
Rocket <adamrivard87@gmail.com>
2025-03-22 12:04:17 -0400

    s

--- button_members_only.sh
+++ button_members_only.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-echo "hell ya"
+# Display the main menu with buttons in the center of the fram
+yad --title="" \
+  --width=600 --height=600 \
+  --form --columns=2 --align=center --no-buttons --dark \
+  --text-align=center --text="<span size='x-large' foreground='gold'>🚀⚡ Welcome to the Power Greeter ⚡🚀</span>\n
+<span size='x-large' foreground='green'>Linux Edition</span>\n\n\n" \
+  --field="<span foreground='#FFFFFF'>$(printf '\uF269')</span>     MX-Spins":FBTN "bash -c 'echo test'" \
+  --field="<span foreground='#FFFFFF'>$(printf '\uF0C7')</span>     Place Holder":FBTN "bash -c 'echo test'"
+
+
+choice=$?
