89f8341ad4b07cbea73308724f50be0f459a933f
Rocket <adamrivard87@gmail.com>
2025-03-22 15:36:47 -0400

    s

--- button_members_mxspins.sh
+++ button_members_mxspins.sh
@@ -5,10 +5,10 @@ yad --title="" \
   --width=600 --height=600 \
   --form --columns=2 --align=center --no-buttons --dark \
   --text-align=center --text="<span size='x-large' foreground='grey'>Select Your Spin</span>\n\n\n" \
-  --field="<span foreground='red'>$(printf '\uF135')</span>      Rocket/Dev Spin":FBTN "bash -c 'echo test'" \
+  --field="<span foreground='red'>$(printf '\U0001F680')</span>      Rocket/Dev Spin":FBTN "bash -c 'echo test'" \
   --field="<span foreground='grey'>$(printf '\U0001F4A3')</span>     Doomsday/Prepper Spin":FBTN "bash -c 'echo test'" \
-  --field="<span foreground='green'>$(printf '\uF11B')</span>     Gaming/Pirate Spin":FBTN "bash -c 'echo test'" \
-  --field="<span foreground='pink'>$(printf '\uF5DC')</span>     Kids/Student Spin":FBTN "bash -c 'echo test'" \
+  --field="<span foreground='green'>$(printf '\U0001F3AE')</span>     Gaming/Pirate Spin":FBTN "bash -c 'echo test'" \
+  --field="<span foreground='pink'>$(printf '\U0001F9E0')</span>     Kids/Student Spin":FBTN "bash -c 'echo test'" \
   --field="<span foreground='blue'>$(printf '\U0001F4DA')</span>     Parent/Teacher Spin":FBTN "bash -c 'echo test'"
 
 choice=$?
