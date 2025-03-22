985868a610b23228d8ae33a295dcdca5ab4d17c5
Rocket <adamrivard87@gmail.com>
2025-03-22 15:14:17 -0400

    s

--- button_members_mxspins.sh
+++ button_members_mxspins.sh
@@ -5,7 +5,7 @@ yad --title="" \
   --width=600 --height=600 \
   --form --columns=2 --align=center --no-buttons --dark \
   --text-align=center --text="<span size='x-large' foreground='grey'>Select Your Spin</span>\n\n\n" \
-  --field="<span foreground='#FFFFFF'>$(printf '\e[91m\uF135\e[0m')</span>     Dev Spin":FBTN "bash -c 'echo test'" \
+  --field="<span foreground='red'>$(printf '\uF135')</span>      Dev Spin":FBTN "bash -c 'echo test'" \
   --field="<span foreground='#FFFFFF'>$(printf '\uF0C7')</span>     Doomsday Spin":FBTN "bash -c 'echo test'" \
   --field="<span foreground='#FFFFFF'>$(printf '\uF0C7')</span>     Retro/Pirate Spin":FBTN "bash -c 'echo test'" \
   --field="<span foreground='#FFFFFF'>$(printf '\uF0C7')</span>     Student Spin":FBTN "bash -c 'echo test'" \
