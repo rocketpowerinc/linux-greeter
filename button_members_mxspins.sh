92de36db633b650ec05142bd6843b404a1d5c503
Rocket <adamrivard87@gmail.com>
2025-03-22 15:30:27 -0400

    s

--- button_members_mxspins.sh
+++ button_members_mxspins.sh
@@ -6,9 +6,9 @@ yad --title="" \
   --form --columns=2 --align=center --no-buttons --dark \
   --text-align=center --text="<span size='x-large' foreground='grey'>Select Your Spin</span>\n\n\n" \
   --field="<span foreground='red'>$(printf '\uF135')</span>      Rocket/Dev Spin":FBTN "bash -c 'echo test'" \
-  --field="<span foreground='grey'>$(printf '\u1F4A3')</span>     Doomsday/Prepper Spin":FBTN "bash -c 'echo test'" \
+  --field="<span foreground='grey'>$(printf '\U0001F4A3')</span>     Doomsday/Prepper Spin":FBTN "bash -c 'echo test'" \
   --field="<span foreground='green'>$(printf '\uF11B')</span>     Gaming/Pirate Spin":FBTN "bash -c 'echo test'" \
   --field="<span foreground='pink'>$(printf '\uF5DC')</span>     Kids/Student Spin":FBTN "bash -c 'echo test'" \
-  --field="<span foreground='blue'>$(printf '\u1F43EB')</span>     Parent/Teacher Spin":FBTN "bash -c 'echo test'"
+  --field="<span foreground='blue'>$(printf '\U0001F43B')</span>     Parent/Teacher Spin":FBTN "bash -c 'echo test'"
 
 choice=$?
