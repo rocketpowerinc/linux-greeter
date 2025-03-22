bcb13dc50798020d7b9dafb0a02cccac1d0e1239
Rocket <adamrivard87@gmail.com>
2025-03-22 15:09:57 -0400

    s

---
+++ button_members_mxspins.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Display the main menu with buttons in the center of the fram
+yad --title="" \
+  --width=600 --height=600 \
+  --form --columns=2 --align=center --no-buttons --dark \
+  --text-align=center --text="<span size='x-large' foreground='grey'>Select Your Spin</span>\n\n\n" \
+  --field="<span foreground='#FFFFFF'>$(printf '\e[91m\uF135\e[0m')</span>     Dev Spin":FBTN "bash -c 'echo test'" \
+  --field="<span foreground='#FFFFFF'>$(printf '\uF0C7')</span>     Doomsday Spin":FBTN "bash -c 'echo test'" \
+  --field="<span foreground='#FFFFFF'>$(printf '\uF0C7')</span>     Retro/Pirate Spin":FBTN "bash -c 'echo test'" \
+  --field="<span foreground='#FFFFFF'>$(printf '\uF0C7')</span>     Student Spin":FBTN "bash -c 'echo test'" \
+  --field="<span foreground='#FFFFFF'>$(printf '\uF0C7')</span>     Teacher Spin":FBTN "bash -c 'echo test'"
+
+choice=$?
