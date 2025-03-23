99e7673ded0275224d0abb67072c8535424c5b3a
Rocket <adamrivard87@gmail.com>
2025-03-23 12:12:06 -0400

    d

--- button_dotfiles_menu.sh
+++ button_dotfiles_menu.sh
@@ -11,14 +11,23 @@ yad --title="" \
   --text-align=center --text="<span size='x-large'>Dotfiles Menu</span>\n\n\n" \
   --field="♻️     Refresh Dotfiles":FBTN "bash -c 'mkdir -p \"$HOME/Github\" && rm -rf \"$HOME/Github/dotfiles\" && git clone https://github.com/rocketpowerinc/dotfiles \"$HOME/Github/dotfiles\" && echo -e \"\e[32mAll dotfiles refreshed!\e[0m\"'" \
   --field="<span foreground='#FFFF00'>$(printf '\u270E')</span>     View Dotfiles":FBTN "bash -c '$DOWNLOAD_PATH/button_dotfiles_open.sh'" \
-  --field="<span foreground='#00FF00'>$(printf '\uF0C5')</span>     Source bashrc":FBTN "bash -c 'grep -qxF \"source \\\"$DOTFILES/bash/bashrc\\\"\" ~/.bashrc || echo \"source \\\"$DOTFILES/bash/bashrc\\\"\" >> ~/.bashrc; echo -e \"\e[32m.bashrc has been copied!\e[0m\"'" \
-  --field="<span foreground='#00FF00'>$(printf '\uF0C5')</span>     Source zshrc":FBTN "bash -c 'grep -qxF \"source \\\"$DOTFILES/zsh/zshrc\\\"\" ~/.zshrc || echo \"source \\\"$DOTFILES/zsh/zshrc\\\"\" >> ~/.zshrc; echo -e \"\e[32m.zshrc has been copied!\e[0m\"'" \
+  --field="<span foreground='#00FF00'>$(printf '\uF0C5')</span>     Source bashrc":FBTN "bash -c 'if grep -qxF \"source \\\"$DOTFILES/bash/bashrc\\\"\" ~/.bashrc; then \
+    echo -e \"\e[33m.bashrc is already sourced!\e[0m\"; \
+  else \
+    echo \"source \\\"$DOTFILES/bash/bashrc\\\"\" >> ~/.bashrc; \
+    echo -e \"\e[32m.bashrc has been sourced!\e[0m\"; \
+  fi'" \
+  --field="<span foreground='#00FF00'>$(printf '\uF0C5')</span>     Source zshrc":FBTN "bash -c 'if grep -qxF \"source \\\"$DOTFILES/zsh/zshrc\\\"\" ~/.zshrc; then \
+    echo -e \"\e[33m.zshrc is already sourced!\e[0m\"; \
+  else \
+    echo \"source \\\"$DOTFILES/zsh/zshrc\\\"\" >> ~/.zshrc; \
+    echo -e \"\e[32m.zshrc has been sourced!\e[0m\"; \
+  fi'" \
   --field="<span foreground='#00FF00'>$(printf '\uF0C5')</span>     Source pwsh profile.ps1":FBTN "pwsh -c \"if (-not (Test-Path -Path $HOME/.config/powershell/profile.ps1)) { \
     New-Item -ItemType File -Path $HOME/.config/powershell/profile.ps1 -Force | Out-Null }; \
-    if (-not (Get-Content $HOME/.config/powershell/profile.ps1 | \
-    Select-String -Pattern '\\. \\\"$env:HOME/Github/dotfiles/pwsh/profile.ps1\\\"')) { \
+    if (-not (Get-Content $HOME/.config/powershell/profile.ps1 | Select-String -SimpleMatch '. \\\"$HOME/Github/dotfiles/pwsh/profile.ps1\\\"')) { \
     Add-Content -Path $HOME/.config/powershell/profile.ps1 -Value '. \\\"$HOME/Github/dotfiles/pwsh/profile.ps1\\\"'; \
-    Write-Host -ForegroundColor Green 'pwsh profile has been copied!' } else { \
+    Write-Host -ForegroundColor Green 'pwsh profile has been sourced!' } else { \
     Write-Host -ForegroundColor Yellow 'pwsh profile already sourced!' }\"" \
   --field="❌ Exit":FBTN "bash -c 'pkill yad'"
 
