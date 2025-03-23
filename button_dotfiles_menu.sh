6d4c0999ea7775328a3a03e75125b80e069143e8
Rocket <adamrivard87@gmail.com>
2025-03-23 10:24:38 -0400

    s

--- button_dotfiles_menu.sh
+++ button_dotfiles_menu.sh
@@ -13,10 +13,10 @@ yad --title="" \
   --width=600 --height=600 \
   --form --columns=2 --align=center --no-buttons --dark \
   --text-align=center --text="<span size='x-large'>Dotfiles Menu</span>\n\n\n" \
-  --field="🌪️     View Dotfiles":FBTN "bash -c '$DOWNLOAD_PATH/button_dotfiles_open.sh'" \
-  --field="<span foreground='#FFFFFF'>$(printf "\uF0C5\n")</span>      Copy bashrc":FBTN "bash -c '$DOTFILES/bash/bashrc'" \
-  --field="🚧     Copy zshrc":FBTN "bash -c '$DOTFILES/bash/bashrc'" \
-  --field="🚧     Copy pwsh profile.ps1":FBTN "bash -c '$DOTFILES/pwsh/profile.ps1'" \
+  --field="<span foreground='#FFFF00'>$(printf "\u270E\n")</span>     View Dotfiles":FBTN "bash -c '$DOWNLOAD_PATH/button_dotfiles_open.sh'" \
+  --field="<span foreground='#00FF00'>$(printf "\uF0C5\n")</span>     Copy bashrc":FBTN "bash -c '$DOTFILES/bash/bashrc'" \
+  --field="<span foreground='#00FF00'>$(printf "\uF0C5\n")</span>     Copy zshrc":FBTN "bash -c '$DOTFILES/bash/bashrc'" \
+  --field="<span foreground='#00FF00'>$(printf "\uF0C5\n")</span>     Copy pwsh profile.ps1":FBTN "bash -c '$DOTFILES/pwsh/profile.ps1'" \
   --field="❌ Exit":FBTN "bash -c 'pkill yad'"
 
 choice=$?
