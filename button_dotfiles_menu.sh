ec8dda1f1467b128c82643538fea24e614ceb4f8
Rocket <adamrivard87@gmail.com>
2025-03-23 11:45:32 -0400

    d

--- button_dotfiles_menu.sh
+++ button_dotfiles_menu.sh
@@ -13,7 +13,7 @@ yad --title="" \
   --field="<span foreground='#FFFF00'>$(printf "\u270E")</span>     View Dotfiles":FBTN "bash -c '$DOWNLOAD_PATH/button_dotfiles_open.sh'" \
   --field="<span foreground='#00FF00'>$(printf '\uF0C5')</span>     Source bashrc":FBTN "bash -c 'grep -qxF \"source \\\"$DOTFILES/bash/bashrc\\\"\" ~/.bashrc || echo \"source \\\"$DOTFILES/bash/bashrc\\\"\" >> ~/.bashrc; echo -e \"\e[32m.bashrc has been copied!\e[0m\"'" \
   --field="<span foreground='#00FF00'>$(printf "\uF0C5")</span>     Source zshrc":FBTN "bash -c 'grep -qxF \"source \\\"$DOTFILES/zsh/zshrc\\\"\" ~/.zshrc || echo \"source \\\"$DOTFILES/zsh/zshrc\\\"\" >> ~/.zshrc; echo -e \"\e[32m.zshrc has been copied!\e[0m\"'" \
-  --field="<span foreground='#00FF00'>$(printf '\uF0C5')</span>     Source pwsh profile.ps1":FBTN "pwsh -c \"if (-not (Get-Content $PROFILE.CurrentUserAllHosts | Select-String -Pattern '\\. \\\"$env:HOME/Github/dotfiles/pwsh/profile.ps1\\\"')) { Add-Content -Path $PROFILE.CurrentUserAllHosts -Value '\\n. \\\"$env:HOME/Github/dotfiles/pwsh/profile.ps1\\\"\\n'; Write-Host -ForegroundColor Green 'pwsh profile has been copied!' } else { Write-Host -ForegroundColor Yellow 'pwsh profile already exists!' }\"" \
+  --field="<span foreground='#00FF00'>$(printf '\uF0C5')</span>     Source pwsh profile.ps1":FBTN "pwsh -c \"if (-not (Get-Content $PROFILE.CurrentUserAllHosts | Select-String -Pattern '\\. \\\"$env:HOME/Github/dotfiles/pwsh/profile.ps1\\\"')) { Add-Content -Path $PROFILE.CurrentUserAllHosts -Value '. \\\"$env:HOME/Github/dotfiles/pwsh/profile.ps1\\\"'; Write-Host -ForegroundColor Green 'pwsh profile has been copied!' } else { Write-Host -ForegroundColor Yellow 'pwsh profile already exists!' }\"" \
   --field="❌ Exit":FBTN "bash -c 'pkill yad'"
 
 choice=$?
