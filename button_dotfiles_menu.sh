f3505e2c66596643a88fe38bae78d9614cbc43e7
Rocket <adamrivard87@gmail.com>
2025-03-23 11:29:50 -0400

    s

--- button_dotfiles_menu.sh
+++ button_dotfiles_menu.sh
@@ -12,8 +12,8 @@ yad --title="" \
   --field="♻️     Refresh Dotfiles":FBTN "bash -c 'mkdir -p \"$HOME/Github\" && rm -rf \"$HOME/Github/dotfiles\" && git clone https://github.com/rocketpowerinc/dotfiles \"$HOME/Github/dotfiles\" && echo -e \"\e[32mAll dotfiles refreshed!\e[0m\"'" \
   --field="<span foreground='#FFFF00'>$(printf "\u270E")</span>     View Dotfiles":FBTN "bash -c '$DOWNLOAD_PATH/button_dotfiles_open.sh'" \
   --field="<span foreground='#00FF00'>$(printf '\uF0C5')</span>     Source bashrc":FBTN "bash -c 'grep -qxF \"source \\\"$DOTFILES/bash/bashrc\\\"\" ~/.bashrc || echo \"source \\\"$DOTFILES/bash/bashrc\\\"\" >> ~/.bashrc; echo -e \"\e[32m.bashrc has been copied!\e[0m\"'" \
-  --field="<span foreground='#00FF00'>$(printf "\uF0C5")</span>     Source zshrc":FBTN "bash -c '$DOTFILES/bash/bashrc'" \
-  --field="<span foreground='#00FF00'>$(printf "\uF0C5")</span>     Source pwsh profile.ps1":FBTN "bash -c '$DOTFILES/pwsh/profile.ps1'" \
+  --field="<span foreground='#00FF00'>$(printf "\uF0C5")</span>     Source zshrc":FBTN "bash -c 'grep -qxF \"source \\\"$DOTFILES/zsh/zshrc\\\"\" ~/.zshrc || echo \"source \\\"$DOTFILES/zsh/zshrc\\\"\" >> ~/.zshrc; echo -e \"\e[32m.zshrc has been copied!\e[0m\"'" \
+  --field="<span foreground='#00FF00'>$(printf "\uF0C5")</span>     Source pwsh profile.ps1":FBTN "bash -c 'grep -qxF \"source \\\"$DOTFILES/pwsh/profile.ps1\\\"\" $PROFILE.CurrentUserAllHosts || echo \"source \\\"$DOTFILES/pwsh/profile.ps1\\\"\" >> ~/.bashrc; echo -e \"\e[32mpwsh profile has been copied!\e[0m\"'" \
   --field="❌ Exit":FBTN "bash -c 'pkill yad'"
 
 choice=$?
