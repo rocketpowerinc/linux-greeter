a1056dea290af08936093105aa399142720902ef
Rocket <adamrivard87@gmail.com>
2025-03-23 12:06:57 -0400

    s

--- button_dotfiles_menu.sh
+++ button_dotfiles_menu.sh
@@ -9,17 +9,10 @@ yad --title="" \
   --width=600 --height=600 \
   --form --columns=2 --align=center --no-buttons --dark \
   --text-align=center --text="<span size='x-large'>Dotfiles Menu</span>\n\n\n" \
-  --field="♻️     Refresh Dotfiles":FBTN "bash -c 'mkdir -p \"$HOME/Github\" && \
-    rm -rf \"$HOME/Github/dotfiles\" && \
-    git clone https://github.com/rocketpowerinc/dotfiles \"$HOME/Github/dotfiles\" && \
-    echo -e \"\e[32mAll dotfiles refreshed!\e[0m\"'" \
+  --field="♻️     Refresh Dotfiles":FBTN "bash -c 'mkdir -p \"$HOME/Github\" && rm -rf \"$HOME/Github/dotfiles\" && git clone https://github.com/rocketpowerinc/dotfiles \"$HOME/Github/dotfiles\" && echo -e \"\e[32mAll dotfiles refreshed!\e[0m\"'" \
   --field="<span foreground='#FFFF00'>$(printf '\u270E')</span>     View Dotfiles":FBTN "bash -c '$DOWNLOAD_PATH/button_dotfiles_open.sh'" \
-  --field="<span foreground='#00FF00'>$(printf '\uF0C5')</span>     Source bashrc":FBTN "bash -c 'grep -qxF \"source \\\"$DOTFILES/bash/bashrc\\\"\" ~/.bashrc || \
-    echo \"source \\\"$DOTFILES/bash/bashrc\\\"\" >> ~/.bashrc; \
-    echo -e \"\e[32m.bashrc has been copied!\e[0m\"'" \
-  --field="<span foreground='#00FF00'>$(printf '\uF0C5')</span>     Source zshrc":FBTN "bash -c 'grep -qxF \"source \\\"$DOTFILES/zsh/zshrc\\\"\" ~/.zshrc || \
-    echo \"source \\\"$DOTFILES/zsh/zshrc\\\"\" >> ~/.zshrc; \
-    echo -e \"\e[32m.zshrc has been copied!\e[0m\"'" \
+  --field="<span foreground='#00FF00'>$(printf '\uF0C5')</span>     Source bashrc":FBTN "bash -c 'grep -qxF \"source \\\"$DOTFILES/bash/bashrc\\\"\" ~/.bashrc || echo \"source \\\"$DOTFILES/bash/bashrc\\\"\" >> ~/.bashrc; echo -e \"\e[32m.bashrc has been copied!\e[0m\"'" \
+  --field="<span foreground='#00FF00'>$(printf '\uF0C5')</span>     Source zshrc":FBTN "bash -c 'grep -qxF \"source \\\"$DOTFILES/zsh/zshrc\\\"\" ~/.zshrc || echo \"source \\\"$DOTFILES/zsh/zshrc\\\"\" >> ~/.zshrc; echo -e \"\e[32m.zshrc has been copied!\e[0m\"'" \
   --field="<span foreground='#00FF00'>$(printf '\uF0C5')</span>     Source pwsh profile.ps1":FBTN "pwsh -c \"if (-not (Test-Path -Path $HOME/.config/powershell/profile.ps1)) { \
     New-Item -ItemType File -Path $HOME/.config/powershell/profile.ps1 -Force | Out-Null }; \
     if (-not (Get-Content $HOME/.config/powershell/profile.ps1 | \
