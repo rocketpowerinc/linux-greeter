f571e3f33ec19fa10131351262fb708b244dfb98
Rocket <adamrivard87@gmail.com>
2025-03-23 11:30:29 -0400

    s

--- button_dotfiles_menu.sh
+++ button_dotfiles_menu.sh
@@ -13,7 +13,7 @@ yad --title="" \
   --field="<span foreground='#FFFF00'>$(printf "\u270E")</span>     View Dotfiles":FBTN "bash -c '$DOWNLOAD_PATH/button_dotfiles_open.sh'" \
   --field="<span foreground='#00FF00'>$(printf '\uF0C5')</span>     Source bashrc":FBTN "bash -c 'grep -qxF \"source \\\"$DOTFILES/bash/bashrc\\\"\" ~/.bashrc || echo \"source \\\"$DOTFILES/bash/bashrc\\\"\" >> ~/.bashrc; echo -e \"\e[32m.bashrc has been copied!\e[0m\"'" \
   --field="<span foreground='#00FF00'>$(printf "\uF0C5")</span>     Source zshrc":FBTN "bash -c 'grep -qxF \"source \\\"$DOTFILES/zsh/zshrc\\\"\" ~/.zshrc || echo \"source \\\"$DOTFILES/zsh/zshrc\\\"\" >> ~/.zshrc; echo -e \"\e[32m.zshrc has been copied!\e[0m\"'" \
-  --field="<span foreground='#00FF00'>$(printf "\uF0C5")</span>     Source pwsh profile.ps1":FBTN "bash -c 'grep -qxF \"source \\\"$DOTFILES/pwsh/profile.ps1\\\"\" $PROFILE.CurrentUserAllHosts || echo \"source \\\"$DOTFILES/pwsh/profile.ps1\\\"\" >> ~/.bashrc; echo -e \"\e[32mpwsh profile has been copied!\e[0m\"'" \
+  --field="<span foreground='#00FF00'>$(printf "\uF0C5")</span>     Source pwsh profile.ps1":FBTN "bash -c 'grep -qxF \"source \\\"$DOTFILES/pwsh/profile.ps1\\\"\" $PROFILE.CurrentUserAllHosts || echo \"source \\\"$DOTFILES/pwsh/profile.ps1\\\"\" >> $PROFILE.CurrentUserAllHosts; echo -e \"\e[32mpwsh profile has been copied!\e[0m\"'" \
   --field="❌ Exit":FBTN "bash -c 'pkill yad'"
 
 choice=$?
