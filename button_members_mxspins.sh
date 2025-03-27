7deba151b7a5e7dbfa98e2011d49465e5fb5c99f
Rocket <adamrivard87@gmail.com>
2025-03-27 15:43:00 -0400

    d

--- button_members_mxspins.sh
+++ button_members_mxspins.sh
@@ -9,10 +9,10 @@ yad --title="" \
   --form --columns=2 --align=center --no-buttons --dark \
   --text-align=center --text="<span size='x-large'>Select Your Spin</span>\n\n\n" \
   --field="🚀      Rocket/Dev Spin":FBTN "bash -c 'echo test'" \
-  --field="💣     Doomsday/Prepper Spin":FBTN "bash -c 'echo test'" \
+  --field="💣     Doomsday/Health Spin":FBTN "bash -c 'echo test'" \
   --field="🎮     Gaming/Pirate Spin":FBTN "bash -c 'echo test'" \
   --field="🧠     Kids/Student Spin":FBTN "bash -c 'echo test'" \
-  --field="📚     Parent/Teacher Spin":FBTN "bash -c 'echo test'" \
+  --field="📚     Parents/Teacher Spin":FBTN "bash -c 'echo test'" \
   --field="📚     Assets":FBTN "bash -c '$DOWNLOAD_PATH/button_members_mxspins_assets.sh'" \
   --field="❌ Exit":FBTN "bash -c 'pkill yad'"
 
