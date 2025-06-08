67cf9370e25eac0655d7af9abdbbda1d055afeeb
Rocket <adamrivard87@gmail.com>
2025-06-08 19:23:01 -0400

    d

--- button_docker_menu.sh
+++ button_docker_menu.sh
@@ -13,7 +13,7 @@ selfhost_filebrowser() {
   mkdir -p "$BASE_DIR"
 
   # Write the docker-compose.yml file
-  cat > "$COMPOSE_FILE" <<EOF
+  cat >"$COMPOSE_FILE" <<EOF
 services:
   filebrowser:
     image: hurlenko/filebrowser:latest
@@ -42,14 +42,52 @@ EOF
 
 export -f selfhost_filebrowser
 
-##############################################
+#########################################################
+
+selfhost_lazydocker() {
+  BASE_DIR="$HOME/Docker/lazydocker"
+  COMPOSE_FILE="$BASE_DIR/docker-compose.yml"
+
+  # Ensure the base directory exists
+  mkdir -p "$BASE_DIR"
+
+  # Write the docker-compose.yml file
+  cat >"$COMPOSE_FILE" <<EOF
+services:
+  lazydocker:
+    build:
+      context: https://github.com/jesseduffield/lazydocker.git
+      args:
+        BASE_IMAGE_BUILDER: golang
+        GOARCH: amd64
+        GOARM:
+    image: lazyteam/lazydocker
+    container_name: lazydocker
+    stdin_open: true
+    tty: true
+    volumes:
+      - /var/run/docker.sock:/var/run/docker.sock
+      - ./config:/.config/jesseduffield/lazydocker
+EOF
+
+  # Navigate to the directory and build + run the container
+  cd "$BASE_DIR" || return
+  sudo docker compose up -d --build
+
+  # Notify the user
+  yad --info --text="Lazydocker is now running. Attach with:\n\n<b>sudo docker exec -it lazydocker lazydocker</b>"
+}
+
+export -f selfhost_lazydocker
+
+#!######################      MENU         #######################
 # Display the main menu with buttons in the center of the frame
 yad --title="" \
   --width=600 --height=600 \
   --form --columns=2 --align=center --no-buttons --dark \
   --text-align=center --text="<span size='x-large'>Docker Menu</span>\n\n\n" \
   --field="🌪️     Selfhost Filebrowser":FBTN "bash -c 'selfhost_filebrowser'" \
-  --field="🚧     Place Holder":FBTN "bash -c 'echo test'" \
+  --field="🚧     Selfhost Lazydocker":FBTN "bash -c 'selfhost_lazydocker'" \
   --field="❌ Exit":FBTN "bash -c 'pkill yad'"
 
 choice=$?
