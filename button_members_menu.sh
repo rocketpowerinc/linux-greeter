9ff478928185bbbf0a6ca87a4a9209a0e1a25146
Rocket <adamrivard87@gmail.com>
2025-06-15 19:29:41 -0400

    d

--- button_members_menu.sh
+++ button_members_menu.sh
@@ -3,7 +3,7 @@
 DOWNLOAD_PATH="$HOME/Downloads/linux-greeter"
 
 # Display the main menu with buttons in the center of the frame
-yad --title="" \
+exec yad --title="" \
   --width=600 --height=600 \
   --form --columns=2 --align=center --no-buttons --dark \
   --text-align=center --text="<span size='x-large'>Members Menu</span>\n\n\n" \
