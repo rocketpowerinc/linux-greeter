41fdcbae51a8c7932ce360381c096da927182f22
Rocket <adamrivard87@gmail.com>
2025-06-15 19:24:23 -0400

    s

--- button_members_websites.sh
+++ button_members_websites.sh
@@ -3,7 +3,7 @@
 DOWNLOAD_PATH="$HOME/Downloads/linux-greeter"
 
 # Create an inline shell script to open multiple websites
-WEBSITES=$(cat << 'EOF'
+PIRATE_WEBSITES=$(cat << 'EOF'
 xdg-open https://www.rottentomatoes.com/ &
 xdg-open https://ext.to/ &
 xdg-open https://torrentgalaxy.one/ &
@@ -19,7 +19,7 @@ yad --title="" \
   --width=600 --height=600 \
   --form --columns=2 --align=center --no-buttons --dark \
   --text-align=center --text="<span size='x-large'>Favorite Websites Menu</span>\n\n\n" \
-  --field="🏴‍☠️     Websites":FBTN "bash -c \"$WEBSITES\"" \
+  --field="🏴‍☠️     Websites":FBTN "bash -c \"$PIRATE_WEBSITES\"" \
   --field="❌ Exit":FBTN "bash -c 'pkill yad'"
 
 choice=$?
