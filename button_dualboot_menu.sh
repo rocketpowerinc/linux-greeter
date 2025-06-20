18a1a71012277e73b2347345ea63dca14cbcd6a3
Rocket <adamrivard87@gmail.com>
2025-06-20 19:47:31 -0400

    s

--- button_dualboot_menu.sh
+++ button_dualboot_menu.sh
@@ -8,20 +8,25 @@ install_refind() {
   sudo apt update
   sudo apt install -y refind
 }
+
+# Export the function so yad can call it
 export -f install_refind
 
-# Write the theme install script to a file
-cat >/tmp/install_matrix_theme.sh <<'EOF'
-THEME_DIR="/boot/efi/EFI/refind/themes/Matrix-rEFInd"
-if [ -d "$THEME_DIR" ] && [ "$(ls -A "$THEME_DIR")" ]; then
-  echo -e "\033[0;32mTheme already exists\033[0m"
-  read -p "Press Enter to exit..."
-  exit 0
-fi
-sudo mkdir -p /boot/efi/EFI/refind/themes
-sudo git clone https://github.com/Yannis4444/Matrix-rEFInd.git "$THEME_DIR"
-EOF
-chmod +x /tmp/install_matrix_theme.sh
+install_matrix_theme() {
+  local THEME_DIR="/boot/efi/EFI/refind/themes/Matrix-rEFInd"
+
+  if [ -d "$THEME_DIR" ] && [ "$(ls -A "$THEME_DIR")" ]; then
+    # Folder exists and is not empty
+    echo -e "\e[32mTheme already exists.\e[0m"
+    read -p "Press Enter to exit..."
+    return 0
+  fi
+
+  # Otherwise, create folder and clone
+  sudo mkdir -p "$THEME_DIR"
+  sudo git clone https://github.com/Yannis4444/Matrix-rEFInd.git "$THEME_DIR"
+}
+
 
 ############################################################################
 yad --title="Installer Menu" \
@@ -29,5 +34,5 @@ yad --title="Installer Menu" \
   --form --columns=2 --align=center --no-buttons --dark \
   --text-align=center --text="<span size='x-large'>Installer Menu</span>\n\n\n" \
   --field="📥  Install rEFInd":FBTN "bash -c install_refind" \
-  --field="📥  Install rEFInd matrix theme":FBTN "bash /tmp/install_matrix_theme.sh" \
+  --field="📥  Install rEFInd matrix theme":FBTN "bash -c install_matrix_theme" \
   --field="❌ Exit":FBTN "bash -c 'pkill yad'"
