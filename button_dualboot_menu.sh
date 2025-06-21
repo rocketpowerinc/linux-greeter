c04630b015bca916033be3a67a636b875aed0d18
Rocket <adamrivard87@gmail.com>
2025-06-20 20:06:20 -0400

    s

--- button_dualboot_menu.sh
+++ button_dualboot_menu.sh
@@ -50,7 +50,7 @@ yad --title="Installer Menu" \
   --width=600 --height=600 \
   --form --columns=2 --align=center --no-buttons --dark \
   --text-align=center --text="<span size='x-large'>Installer Menu</span>\n\n\n" \
-  --field="📥  Install rEFInd":FBTN "bash -c install_refind" \
+  --field="🔎  Install rEFInd":FBTN "bash -c install_refind" \
   --field="📥  Install rEFInd matrix theme":FBTN "bash -c install_matrix_theme" \
   --field="📥  Install rEFInd gruvbox theme":FBTN "bash -c install_gruvbox_theme" \
   --field="❌ Exit":FBTN "bash -c 'pkill yad'"
