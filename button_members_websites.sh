2c12297f3d039523c7b4b34fbdd166764c9198e8
rocket <adamrivard87@gmail.com>
2025-07-12 20:23:41 -0400

    s

--- button_members_websites.sh
+++ button_members_websites.sh
@@ -2,6 +2,7 @@
 
 DOWNLOAD_PATH="$HOME/Downloads/linux-greeter"
 
+#######################################
 # Create an inline shell script to open multiple websites
 PIRATE_WEBSITES=$(cat << 'EOF'
 xdg-open https://www.rottentomatoes.com/ &
@@ -19,6 +20,15 @@ sleep 1s
 xdg-open https://thepiratebay10.xyz/ &
 EOF
 )
+################################
+FIREFOX_BOOKMARKS=$(
+curl -L -o "$HOME/Downloads/PRIVATE-bookmarks-2025-07-12.json" https://raw.githubusercontent.com/rocketpowerinc/appbundles/main/Firefox-Bookmarks/PRIVATE-bookmarks-2025-07-12.json
+)
+
+
+
+
+#!######################
 
 # Display the main menu
 yad --title="" \
@@ -26,6 +36,7 @@ yad --title="" \
   --form --columns=2 --align=center --no-buttons --dark \
   --text-align=center --text="<span size='x-large'>Favorite Websites Menu</span>\n\n\n" \
   --field="🏴‍☠️     Pirating":FBTN "bash -c \"$PIRATE_WEBSITES\"" \
+  --field="🦊     Firefox Bookmarks":FBTN "bash -c \"$FIREFOX_BOOKMARKS\"" \
   --field="❌ Exit":FBTN "bash -c 'pkill yad'"
 
 choice=$?
